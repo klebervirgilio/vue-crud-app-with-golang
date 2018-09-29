d119afc65dd72cee13b90b18c806423c756a83cb
Kleber Correia <klebervirgilio@gmail.com>
2018-09-29 22:56:30 +0100

    better api and better layout

--- pkg/http/web/app/src/App.vue
+++ pkg/http/web/app/src/App.vue
@@ -8,59 +8,103 @@
         hide-details
         label="Search for your OOS project on Github + Press Enter"
         prepend-inner-icon="search"
+        v-model="query"
         @keyup.enter="search"
-        value="vuejs"
       ></v-text-field>
       <v-spacer></v-spacer>
+       <v-tabs
+        slot="extension"
+        v-model="tabs"
+        centered
+        color="teal"
+        slider-color="white"
+      >
+        <v-tab :key="1">
+          SEARCH
+        </v-tab>
+        <v-tab :key="2">
+          KUDOS
+        </v-tab>
+      </v-tabs>
     </v-toolbar>
-    <v-container fluid class="teal lighten-4">
-      <v-layout>
-        <v-flex> 
-          <div class="text-xs-center">
-            <v-btn v-if="loved" slot="activator" @click.prevent="loved = false" outline color="white lighten-2">Show All</v-btn>
-            <v-btn v-else slot="activator" @click.prevent="loved = true" outline color="white lighten-2">Show My Kudos</v-btn>
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-container>
       
-    <v-container grid-list-md fluid class="grey lighten-4">
-      <v-layout row wrap>
-        <v-flex v-for="repo in filteredRepos()" :key="repo.id" md4 xs12 sm12>
-          <v-card >
-            <v-card-title primary-title>
-              <div class="repo-card-content">
-                <h3 class="headline mb-0">{{repo.full_name}}</h3>
-                <div>{{repo.description}}</div>
-              </div>
-            </v-card-title>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-dialog v-if="isLiked(repo)">
-                <v-btn slot="activator" icon flat color="orange lighten-2"><v-icon>notes</v-icon></v-btn>
-                <v-card>
-                  <v-card-title class="headline white--text teal lighten-2" primary-title>Notes</v-card-title>
-                  <v-card-text>
-                    <v-textarea></v-textarea>
-                  </v-card-text>
-                  <v-divider></v-divider>
+    <v-container grid-list-md fluid class="grey lighten-4" >
+        <v-tabs-items style="width:100%" v-model="tabs">
+          <v-tab-item :key="1">
+            <v-layout row wrap>
+              <v-flex v-for="repo in repos" :key="repo.id" md4>
+                <v-card >
+                  <v-card-title primary-title>
+                    <div class="repo-card-content">
+                      <h3 class="headline mb-0">{{repo.full_name}}</h3>
+                      <div>{{repo.description}}</div>
+                    </div>
+                  </v-card-title>
                   <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="orange" flat @click="dialog = false">
-                      Save
+                    <v-dialog v-if="isKudo(repo)">
+                      <v-btn slot="activator" icon flat color="orange lighten-2"><v-icon>notes</v-icon></v-btn>
+                      <v-card>
+                        <v-card-title class="headline white--text teal lighten-2" primary-title>Notes</v-card-title>
+                        <v-card-text>
+                          <v-textarea></v-textarea>
+                        </v-card-text>
+                        <v-divider></v-divider>
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn color="orange" flat @click="dialog = false">
+                            Save
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                    <v-btn @click.prevent="toggleKudo(repo)"  flat icon color="pink">
+                      <v-icon v-if="isKudo(repo)">favorite</v-icon>
+                      <v-icon v-else>favorite_border</v-icon>
                     </v-btn>
                   </v-card-actions>
                 </v-card>
-              </v-dialog>
-              <v-btn @click.prevent="toggleLike(repo)"  flat icon color="pink">
-                <v-icon v-if="isLiked(repo)">favorite</v-icon>
-                <v-icon v-else>favorite_border</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
+              </v-flex>
+            </v-layout>
+          </v-tab-item>
+          <v-tab-item :key="2">
+            <v-layout row wrap>
+              <v-flex v-for="repo in kudos" md4 >
+                <v-card >
+                  <v-card-title primary-title>
+                    <div class="repo-card-content">
+                      <h3 class="headline mb-0">{{repo.full_name}}</h3>
+                      <div>{{repo.description}}</div>
+                    </div>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-dialog v-if="isKudo(repo)">
+                      <v-btn slot="activator" icon flat color="orange lighten-2"><v-icon>notes</v-icon></v-btn>
+                      <v-card>
+                        <v-card-title class="headline white--text teal lighten-2" primary-title>Notes</v-card-title>
+                        <v-card-text>
+                          <v-textarea></v-textarea>
+                        </v-card-text>
+                        <v-divider></v-divider>
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn color="orange" flat @click="dialog = false">
+                            Save
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                    <v-btn @click.prevent="toggleKudo(repo)"  flat icon color="pink">
+                      <v-icon v-if="isKudo(repo)">favorite</v-icon>
+                      <v-icon v-else>favorite_border</v-icon>
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-tab-item>
+        </v-tabs-items>
     </v-container>
   </v-app>
 </template>
@@ -70,36 +114,34 @@ export default {
   name: 'App',
   data() {
     return {
-      loved: false,
+      tabs: null,
+      query: 'okta',
       repos: [],
-      favs: [],
+      kudos: [],
     }
   },
   mounted() {
-    this.search({target: {value: 'vuejs'}})
+    this.githubQuery(this.query);
   },
   methods: {
-    filteredRepos(){
-      if (this.loved) {
-        return this.repos.filter( repo => this.favs.includes(repo.id) )
-      }
-      return this.repos;
+    isKudo(repo){
+      return this.kudos.find(r => r.id === repo.id) 
     },
-    isLiked(repo){
-      return this.favs.includes(repo.id)
-    },
-    toggleLike(repo){
-      if (this.isLiked(repo)) {
+    toggleKudo(repo){
+      if (this.isKudo(repo)) {
         return this.dislike(repo)
       }
-      this.favs.push(repo.id);
+      this.kudos.push(repo);
     },
     dislike(repo){
-      const index = this.favs.indexOf(repo.id)
-      this.favs.splice(index, 1);
+      const index = this.kudos.indexOf(repo);
+      this.kudos.splice(index, 1);
     },
-    search(evt) {
-      fetch("https://api.github.com/search/repositories?q=" + evt.target.value)
+    search() {
+      this.githubQuery(this.query);
+    },
+    githubQuery(query) {
+      fetch("https://api.github.com/search/repositories?q=" + query)
         .then(r => r.json())
         .then((r)=> {
           this.repos = r.items;
@@ -114,6 +156,9 @@ export default {
  .repo-card-content {
    height: 90px;
    overflow: scroll;
+ }
+ .v-tabs__content {
+   padding-bottom: 2px;
  }
 </style>
 
